5f3386f53da88a083523b59e3e57e7e23ba5f529
istekhar <istekhararkam25@gmail.com>
2025-05-24 23:00:19 +0530

    footer done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,118 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/Image/Navbar/Logo.svg";
+import { FaLinkedin, FaTwitter, FaFacebookSquare } from "react-icons/fa";
+import { IoCall, IoMail } from "react-icons/io5";
+import { FaLocationDot } from "react-icons/fa6";
+import { RiInstagramFill } from "react-icons/ri";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-[#000000] ">
+      <div className="mx-10  sm:flex sm:flex-wrap sm:space-x-10 sm:justify-center xl:space-x-10 lg:space-x-0 lg:justify-start xl:justify-center">
+        <div className="flex  justify-center py-6 sm:hidden">
+          <img src={Logo} className="h-12 " alt="Logo" />
+        </div>
 
-export default Footer
+        <div className="sm:flex flex-col item-center mt-10 hidden w-68 ">
+          <h1 className="text-xl text-[#3CBDB1] font-bold uppercase">
+            reach us
+          </h1>
+          <div className="flex items-center gap-2">
+            <span className="text-lg">
+              <IoCall />
+            </span>
+            <p className=" py-2">+91-8707727347</p>
+          </div>
+          <div className="flex items-center gap-3">
+            {" "}
+            <span className="text-lg">
+              <IoMail />
+            </span>
+            <p className=" ">hello@toletglobe.in</p>
+          </div>
+          <div className="flex items-center gap-3">
+            {" "}
+            <span className="pb-6 text-lg">
+              <FaLocationDot />
+            </span>{" "}
+            <p className="capitalize py-2">
+              D1/122 Vipulkhand, Gomtinagar Lucknow,Uttar Pradesh
+            </p>
+          </div>
+        </div>
+        <center className="capitalize  text-base sm:hidden">
+          One-stop solution for all your brokerage-free rental needs
+        </center>
+        <div className="flex m-4 justify-between sm:hidden">
+          <p className="text-2xl">
+            <FaLinkedin />
+          </p>
+          <p className="text-2xl">
+            <FaFacebookSquare />
+          </p>
+          <p className="text-2xl">
+            <FaTwitter />
+          </p>
+          <p className="text-2xl">
+            <RiInstagramFill />
+          </p>
+        </div>
+
+        <div className="flex flex-col item-center mt-10 sm:w-68 items-center sm:items-start xl:ml-8 lg:w-62 lg:ml-10 ">
+          <h1 className="text-xl text-[#3CBDB1] font-bold uppercase">
+            quick links
+          </h1>
+          <p className="capitalize py-2">home</p>
+          <p className="capitalize ">blog</p>
+          <p className="capitalize py-2">property</p>
+        </div>
+        <div className=" flex flex-col item-center mt-10 sm:w-68 items-center sm:items-start lg:w-62 ">
+          <h1 className="text-xl text-[#3CBDB1] font-bold uppercase">
+            service
+          </h1>
+          <p className="capitalize py-2">paying guest</p>
+          <p className="capitalize ">flat and house</p>
+          <p className="capitalize py-2">shops and godown</p>
+        </div>
+        <div className="capitalize hidden sm:block sm:w-68 sm:mt-10 ">
+          <img src={Logo} className="h-12 mb-4" alt="Logo" />
+          <p>one-stop solution for brokerage-free rental needs</p>
+        </div>
+        <center className=" flex flex-col item-center mt-10 sm:hidden">
+          <h1 className="text-xl text-[#3CBDB1] font-bold uppercase">
+            reach us
+          </h1>
+          <div className="flex items-center gap-4">
+            <span className="text-2xl">
+              <IoCall />
+            </span>
+            <p className=" py-2">+91-8707727347</p>
+          </div>
+          <div className="flex items-center gap-4">
+            {" "}
+            <span className="text-2xl ">
+              <IoMail />
+            </span>
+            <p className=" ">hello@toletglobe.in</p>
+          </div>
+          <div className="flex items-center  ">
+            {" "}
+            <span className="pb-12 text-2xl">
+              <FaLocationDot />
+            </span>{" "}
+            <p className="capitalize py-2">
+              D1/122 Vipulkhand, Gomtinagar Lucknow,Uttar Pradesh
+            </p>
+          </div>
+        </center>
+      </div>
+      <div className="bg-[#232323] ">
+        <p className="px-6 py-8 text-gray-400">
+          © 2023 To-Let Globe -- Lucknow. All Rights Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/Image/Navbar/Logo.svg";
-
+import { GiHamburgerMenu } from "react-icons/gi";
 const Navbar = () => {
   return (
     <>
       <header className="sticky top-0 z-50 shadow">
-        <nav className="bg-[#232323] border-gray-200 px-4 lg:px-6 py-2.5">
-          <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto">
-            <Link to="/" className="flex items-center">
-              <img src={Logo} className="h-12 mr-3" alt="Logo" />
+        <nav className="bg-[#232323] px-4 lg:px-6 py-2.5">
+          <div className="flex items-center mx-auto  justify-between">
+            <Link to="/" className="lg:flex items-center ml-10 hidden ">
+              <img src={Logo} className="h-12 " alt="Logo" />
             </Link>
-
+            <div className="text-white text-2xl lg:hidden">
+              <GiHamburgerMenu />
+            </div>
+            <div className=" w-full flex justify-center lg:hidden">
+              <img src={Logo} className="h-12" alt="Logo" />
+            </div>
             <div
-              className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1"
-              id="mobile-menu-2"
+              className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1 mr-10"
+              id="mobile-menu-2 "
             >
-              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 xl:mr-6">
                 <li>
                   <NavLink
                     to="/"
                     className={({ isActive }) =>
-                      `block py-2 pr-4 pl-3 duration-200 ${
-                        isActive ? "text-orange-700" : "text-gray-700"
-                      }  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${
-                        isActive ? "text-orange-700" : "text-gray-700"
-                      }`
+                      `capitalize block ${
+                        isActive ? "bg-[#5AA7A0] text-white" : "text-white"
+                      }  lg:border-0 lg:px-3 rounded-lg lg:pb-0.5`
                     }
                   >
                     Home
@@ -35,11 +38,9 @@ const Navbar = () => {
                   <NavLink
                     to="/service"
                     className={({ isActive }) =>
-                      `block py-2 pr-4 pl-3 duration-200 ${
-                        isActive ? "text-orange-700" : "text-white"
-                      }  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${
-                        isActive ? "text-orange-700" : "text-gray-700"
-                      }`
+                      `capitalize block ${
+                        isActive ? "bg-[#5AA7A0] text-white" : "text-white"
+                      }  lg:border-0 lg:px-3 rounded-lg lg:pb-0.5`
                     }
                   >
                     Service
@@ -49,11 +50,9 @@ const Navbar = () => {
                   <NavLink
                     to="/blog"
                     className={({ isActive }) =>
-                      ` block py-2 pr-4 pl-3 duration-200  ${
-                        isActive ? "text-orange-700" : "text-gray-700"
-                      }  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 capitalize ${
-                        isActive ? "text-orange-700" : "text-gray-700"
-                      }`
+                      ` capitalize block ${
+                        isActive ? "bg-[#5AA7A0] text-white" : "text-white"
+                      }  lg:border-0 lg:px-3 rounded-lg lg:pb-0.5`
                     }
                   >
                     blog
@@ -63,11 +62,9 @@ const Navbar = () => {
                   <NavLink
                     to="/contact"
                     className={({ isActive }) =>
-                      `block py-2 pr-4 pl-3 duration-200 ${
-                        isActive ? "text-orange-700" : "text-gray-700"
-                      }  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 capitalize ${
-                        isActive ? "text-orange-700" : "text-gray-700"
-                      }`
+                      `capitalize block ${
+                        isActive ? "bg-[#5AA7A0] text-white" : "text-white"
+                      }  lg:border-0 lg:px-3 rounded-lg lg:pb-0.5`
                     }
                   >
                     contact
@@ -77,11 +74,9 @@ const Navbar = () => {
                   <NavLink
                     to="/about"
                     className={({ isActive }) =>
-                      `block py-2 pr-4 pl-3 duration-200 ${
-                        isActive ? "text-orange-700" : "text-gray-700"
-                      }  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 capitalize ${
-                        isActive ? "text-orange-700" : "text-gray-700"
-                      }`
+                      `capitalize block ${
+                        isActive ? "bg-[#5AA7A0] text-white" : "text-white"
+                      }  lg:border-0 lg:px-3 rounded-lg lg:pb-0.5`
                     }
                   >
                     about
@@ -91,11 +86,9 @@ const Navbar = () => {
                   <NavLink
                     to="/propertyListing"
                     className={({ isActive }) =>
-                      `block py-2 pr-4 pl-3 duration-200 ${
-                        isActive ? "text-orange-700" : "text-gray-700"
-                      }  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 capitalize ${
-                        isActive ? "text-orange-700" : "text-gray-700"
-                      }`
+                      `capitalize block ${
+                        isActive ? "bg-[#5AA7A0] text-white" : "text-white"
+                      }  lg:border-0 lg:px-3 rounded-lg lg:pb-0.5`
                     }
                   >
                     property listing
@@ -105,11 +98,9 @@ const Navbar = () => {
                   <NavLink
                     to="/logIn"
                     className={({ isActive }) =>
-                      `block py-2 pr-4 pl-3 duration-200 ${
-                        isActive ? "text-orange-700" : "text-gray-700"
-                      }  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 capitalize ${
-                        isActive ? "text-orange-700" : "text-gray-700"
-                      }`
+                      `capitalize block ${
+                        isActive ? "bg-[#5AA7A0] text-white" : "text-white"
+                      }  lg:border-0 lg:px-3 rounded-lg lg:pb-0.5`
                     }
                   >
                     log in
